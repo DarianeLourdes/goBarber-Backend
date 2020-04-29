8a39c4ded18335bc5ef69acdda879566c61cdff0
darianelourdes <darianelsantana@gmail.com>
2020-04-29 20:46:07 -0300

    chore: configuring nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'b02cabf50e5e7f',
+    pass: '522983ce735b1c',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>,',
+  },
+};
